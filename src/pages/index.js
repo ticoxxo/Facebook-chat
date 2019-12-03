32eef86ac9960c1c3b81950b9c86643573fcb868
webdevabordo <jalmeida@allabordo.com>
2019-12-03 12:35:51 -0700

    Revert "react for msn"
    This reverts commit 28695a9d9079d738ff4ec5804bcda069a7962d81.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const IndexPage = () => (
   <Layout>
@@ -14,10 +13,6 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-      <MessengerCustomerChat 
-      pageId="843611769430123"
-      appId="843611769430123"
-      />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
